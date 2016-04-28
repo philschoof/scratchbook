00a85843be886da980939b88fb5c45e1a3731acf
philschoof <phil.schoof@gmail.com>
2016-04-27 20:34:11 -0400

    Initial front end commit. Building auth

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// user require with a reference to bundle the file and use it in this file
+// var example = require('./example');
+
+// load manifests
+// scripts
+require('./assets/scripts/index.js');
+
+// styles
+require('./assets/styles/index.scss');
+
+// attach jQuery globally
+require('expose?$!jquery');
+require('expose?jQuery!jquery');
+
+// attach getFormFields globally
+
+require('expose?getFormFields!./lib/get-form-fields.js');
+
+$('.open-signin').on('click', function(e){
+  e.preventDefault();
+  $('#myModal').modal('show');
+});
+
+$('.open-signup').on('click', function(e){
+  e.preventDefault();
+  $('#myModalUp').modal('show');
+});
