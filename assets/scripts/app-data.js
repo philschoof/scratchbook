404b569e8d7c4ec33efdf0edaa7b51b08c6dc549
philschoof <phil.schoof@gmail.com>
2016-05-02 16:46:31 -0400

    Change api for deployment

--- assets/scripts/app-data.js
+++ assets/scripts/app-data.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const app = {
-  api:'http://localhost:3000/',
+  api:'https://serene-spire-31706.herokuapp.com/',
   lastFm:'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=507b5d03b9f50c1d266bde3a5ef48fe0&artist=' //+ preparedArtist + '&album=' + preparedAlbum + '&format=json'
 };
 
