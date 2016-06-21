e6b5299395b67d6ed96acab1cccfe9cde5c54730
philschoof <phil.schoof@gmail.com>
2016-06-21 14:37:44 -0400

    Commit before push to gh-pages

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(145);
+	module.exports = __webpack_require__(150);
 
 
 /***/ },
@@ -9954,7 +9954,7 @@
 
 /***/ },
 
-/***/ 145:
+/***/ 150:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
